13b1f17dffdbcbe99b9a75f82450a83b66e35342
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-04-06 23:55:08 -0400

    [TASK 101] Puppet for setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,5 +1,5 @@
 # Puppet for setup Redo the task #0 but by using Puppet:
 exec { "Preparing_Server":
 command => "sudo apt-get update && sudo apt-get -y install nginx && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && echo 'Holberton School' | sudo tee /data/web_static/releases/test/index.html && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && var='location /hbnb_static {\n\t\talias /data/web_static/current/;}' && sudo sed -i "44i$var" /etc/nginx/sites-available/default && sudo service nginx reload && sudo service nginx restart"
-provider => "shell"
+provider => shell,
 }
