ae1f4b3193bf0b05195c507b5442cb4773172646
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-04-06 23:06:07 -0400

    [SHELL CHECK] Expressions don't expand in single quotes, use double quotes for that.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,6 +8,6 @@ echo 'Holberton School' | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 var='location /hbnb_static {\n\t\talias /data/web_static/current/;}'
-sudo sed -i '44i$var' /etc/nginx/sites-available/default
+sudo sed -i "44i$var" /etc/nginx/sites-available/default
 sudo service nginx reload
 sudo service nginx restart
