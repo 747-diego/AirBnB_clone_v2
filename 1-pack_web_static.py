9b69944a43f68fa709cc99c772a75ab33154bc28
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-04-06 23:23:45 -0400

    [TASK 1] Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive"""
-
-from fabric.api import local
-from datetime import datetime
+"""Write a Fabric script that generates a .tgz archive."""
 import os
+from fabric.api import *
+from datetime import datetime
 
 
 def do_pack():
-    """Generates a .tgz archive"""
-    File = datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
+    """Compress before sending."""
     try:
-        if os.path.exists("versions/") is False:
-            os.mkdir("versions/")
-        local("tar -cvzf versions/web_static_{} web_static".format(File))
-        return File
+        PathOfFile = datetime.now().strftime("%Y%m%d%H%M%S") + '.tgz'
+        os.mkdir('versions/')
+
+        local("tar -fvcz versions/web_static_{} web_static".format(PathOfFile))
     except:
         return None
