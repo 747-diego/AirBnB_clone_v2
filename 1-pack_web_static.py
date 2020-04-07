ba1038ecaa97e05f42ec1fe44e8f988e9f92ffd8
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-04-06 23:26:33 -0400

    [PEP8 CHECK]

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,7 @@ def do_pack():
     try:
         PathOfFile = datetime.now().strftime("%Y%m%d%H%M%S") + '.tgz'
         os.mkdir('versions/')
-
         local("tar -fvcz versions/web_static_{} web_static".format(PathOfFile))
+        return PathOfFile
     except:
         return None
